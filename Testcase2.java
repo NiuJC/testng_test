e6ee4afbf847d9b696dbde07c74ee74c2ec09e8d
NiuJC <44601960+NiuJC@users.noreply.github.com>
2021-04-26 15:44:06 +0800

    Add files via upload

---
+++ Testcase2.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.*;
+
+public class Testcase2 {
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("this is before class for TestCase2");
+    }
+
+    @Test
+    public void TestNgLearn() {
+        System.out.println("this is TestNG test case for TestCase2");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("this is after class for TestCase2");
+    }
+}
